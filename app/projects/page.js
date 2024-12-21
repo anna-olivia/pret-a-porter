bb225a59a4536ce607a796103832282e21643aa2
anna-olivia <a.wietholtz@posteo.de>
2024-12-21 15:31:32 +0100

    projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -4,10 +4,16 @@ import { IoArrowBack } from "react-icons/io5";
 
 const Projects = ()=> {
 const links = [
-    "https://github.com/anna-olivia",
-    "https://github.com/anna-olivia?tab=repositories",
-    "https://github.com/anna-olivia?tab=repositories",
-
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "html5" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "css3" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "bootstrap" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "js" },
+    {href:  "https://github.com/anna-olivia", skill: "175px", text: "react" },
+    {href:  "https://github.com/anna-olivia", skill: "150px", text: "next" },
+    {href:  "https://github.com/anna-olivia", skill: "150px", text: "node.js" },
+    {href:  "https://github.com/anna-olivia", skill: "100px", text: "typescript" },
+    {href:  "https://github.com/anna-olivia", skill: "100px", text: "php" },
+    
 ]
 
 
@@ -19,12 +25,22 @@ const links = [
               <div className={styles.gallery}>
           {links.map((link, index) => 
           (<a
-          href={link}
+          href={link.href}
           key={index}
           target="_blank"
           rel="noopener noreferrer"
         >
-            <div className={styles.project}></div>
+            <div className={styles.project}>
+           
+            <div className={styles.skills} style={{width: `${link.skill}`, height: `${link.skill}`}}>
+                
+                <h2>
+                {link.text}
+
+                </h2>
+                 </div>
+            </div>
+           
         </a>
       )    )
     }
