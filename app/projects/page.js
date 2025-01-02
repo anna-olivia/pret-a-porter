a3fb9764ee78d937b3b31af6b8b037a3fa29cb1c
anna-olivia <a.wietholtz@posteo.de>
2025-01-02 14:53:40 +0100

    projects mobile

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,24 +1,68 @@
+"use client";
+import React, {useState} from "react";
 import Link from "next/link";
 import styles from '../styles/Projects.module.css';
-import { IoArrowForward } from "react-icons/io5";
+import { IoArrowBack, IoInformationCircleOutline, IoClose } from "react-icons/io5";
 
 const Projects = ()=> {
+const links = [
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "html5" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "css3" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "bootstrap" },
+    {href:  "https://github.com/anna-olivia", skill: "200px", text: "js" },
+    {href:  "https://github.com/anna-olivia", skill: "175px", text: "react" },
+    {href:  "https://github.com/anna-olivia", skill: "150px", text: "next" },
+    {href:  "https://github.com/anna-olivia", skill: "150px", text: "node.js" },
+    {href:  "https://github.com/anna-olivia/play-type/tree/master", skill: "100px", text: "typescript" },
+    {href:  "https://github.com/anna-olivia", skill: "100px", text: "php" },
+    
+]
+const [isInfoOpen, setIsInfoOpen] = useState(false);
+
+const toggleInfo = () => {
+  setIsInfoOpen(!isInfoOpen);
+};
+
+
     return (
         <div className={styles.projects}>
-        <h1>Under Construction ...</h1>
-        <Link href="/" ><IoArrowForward className={styles.link} />
-              back home
+<div className={styles.header}>
+<Link href="/" ><IoArrowBack className={styles.link} />
               </Link>
-              <a
-          href="https://github.com/anna-olivia"
+              <IoInformationCircleOutline onClick={toggleInfo} className={styles.info}/>
+             
+</div>
+<div className={`${styles.explain} ${isInfoOpen ? styles.open : ""}`}>
+<IoClose className={styles.icon} onClick={toggleInfo} />
+<p>This is a little sneak peak of some of my projects.</p>
+<p> I chose to visualise the level of my skills in each language with a growing inner circle. The larger the pinkish circle, the more confident I am working with this language.</p>
+<p> So far, I only categorize my skills as beginner, intermediate, and advanced. In the future, I will refine it a bit.</p>
+</div>
+              <div className={styles.gallery}>
+          {links.map((link, index) => 
+          (<a
+          href={link.href}
+          key={index}
           target="_blank"
           rel="noopener noreferrer"
-        >or visit  
-         <span>github
-        </span>
+        >
+            <div className={styles.project}>
+           
+            <div className={styles.skills} style={{width: `${link.skill}`, height: `${link.skill}`}}>
+                
+                <h2>
+                {link.text}
+
+                </h2>
+                 </div>
+            </div>
            
         </a>
-        <br />
+      )    )
+    }
+              </div>
+          
+     
               
         </div>
     )
