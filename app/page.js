69f20e554f0bc6b47aaa2b556208f6bc1bb439cb
anna-olivia <79095533+anna-olivia@users.noreply.github.com>
2025-01-06 11:32:17 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 
 import Hero from "./components/Hero";
-import styles from "./styles/Landing.module.css";
+
 import About from "./components/About";
 export default function Home() {
   return (
     <>
       <Hero />
-      <div className={styles.responsive}>
+     
         <About/> 
-      </div>
+    
     </>
   );
 }
