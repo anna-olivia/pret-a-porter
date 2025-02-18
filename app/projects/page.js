55ee090de46f981b39e18457e995cbe8f00ab5fe
anna-olivia <79095533+anna-olivia@users.noreply.github.com>
2025-02-18 16:32:02 +0100

    Update page.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -6,16 +6,16 @@ import { IoArrowBack, IoInformationCircleOutline, IoClose } from "react-icons/io
 
 const Projects = ()=> {
 const links = [
-{href:  "https://github.com/anna-olivia", skill: "200px", text: "figma" },
-    {href:  "https://github.com/anna-olivia", skill: "200px", text: "html5" },
-    {href:  "https://github.com/anna-olivia", skill: "200px", text: "css3" },
-    {href:  "https://github.com/anna-olivia", skill: "200px", text: "bootstrap" },
-    {href:  "https://github.com/anna-olivia", skill: "200px", text: "js" },
-    {href:  "https://github.com/anna-olivia", skill: "175px", text: "react" },
-    {href:  "https://github.com/anna-olivia", skill: "150px", text: "next" },
-    {href:  "https://github.com/anna-olivia", skill: "150px", text: "node.js" },
+{href:  "https://github.com/anna-olivia/prototyping-figma", skill: "200px", text: "figma" },
+    {href:  "https://github.com/anna-olivia/Spickzettel_gruppeTZ", skill: "200px", text: "html5" },
+    {href:  "https://github.com/anna-olivia/restore2020_wp", skill: "200px", text: "css3" },
+    {href:  "https://github.com/anna-olivia/booti", skill: "200px", text: "bootstrap" },
+    {href:  "https://github.com/anna-olivia/e-my-ud-vite1", skill: "200px", text: "js" },
+    {href:  "https://github.com/anna-olivia/invest-r-action", skill: "175px", text: "react" },
+    {href:  "https://github.com/anna-olivia/basic-router", skill: "150px", text: "next" },
+    {href:  "https://github.com/anna-olivia/back-ground", skill: "150px", text: "node.js" },
     {href:  "https://github.com/anna-olivia/play-type/tree/master", skill: "100px", text: "typescript" },
-    {href:  "https://github.com/anna-olivia", skill: "100px", text: "php" },
+    {href:  "https://github.com/anna-olivia/restore2020_abschluss", skill: "100px", text: "php" },
     
 ]
 const [isInfoOpen, setIsInfoOpen] = useState(false);
